59fe4c883a0be647361d4b12decf224dba61e0ad
Noah A <nanderson815@gmail.com>
2019-11-01 21:06:41 -0400

    adding connect scanner feature

--- Serial/SerialPort.js
+++ Serial/SerialPort.js
@@ -3,19 +3,6 @@ var port = new SerialPort('COM4', 9600);
 var Readline = SerialPort.parsers.Readline
 
 let report = []
-let livePorts;
-
-SerialPort.list()
-    .then((ports) => {
-        livePorts = ports
-    })
-    .catch(err => err)
-
-const refreshPorts = async () => {
-
-}
-
-
 
 var parser = new Readline()
 port.pipe(parser)
@@ -24,13 +11,10 @@ parser.on('data', function (data) {
     report.push(data);
 });
 
-const listPorts = () => {
-    return livePorts
-}
 
 async function getPorts() {
-    livePorts = await SerialPort.list()
-    return livePorts;
+    let livePorts = await SerialPort.list()
+    return livePorts.map(a => a.path);
 }
 
 module.exports.getPorts = getPorts;
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,9 +2,30 @@ import React, { useEffect } from 'react';
 import { withFirebase, isLoaded, isEmpty } from 'react-redux-firebase';
 import { useSelector } from 'react-redux';
 import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import { makeStyles } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 import { Redirect } from 'react-router-dom';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 200,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 const Dashboard = (props) => {
+    const classes = useStyles();
+    const inputLabel = React.useRef(null);
+    const [labelWidth, setLabelWidth] = React.useState(0);
+    React.useEffect(() => {
+        setLabelWidth(inputLabel.current.offsetWidth);
+    }, []);
     const auth = useSelector(state => state.firebase.auth)
 
     const [values, setValues] = React.useState({
@@ -12,8 +33,12 @@ const Dashboard = (props) => {
         location: '',
         name: '',
         port: '',
-        portOptions: ''
+        portOptions: []
     })
+
+    const handleChange = event => {
+        setValues({ ...values, port: event.target.value });
+    };
 
     useEffect(() => {
         if (isLoaded(auth) && !isEmpty(auth)) {
@@ -31,7 +56,12 @@ const Dashboard = (props) => {
 
     const getPorts = () => {
         window.ipcRenderer.once('portList', (event, arg) => {
-            console.log(arg) // prints "pong"
+            if (arg.length > 0) {
+                setValues({ ...values, portOptions: arg })
+                console.log(values.portOptions)
+            } else {
+                console.log("No scanner connected.")
+            }
         })
         window.ipcRenderer.send('getPorts', 'portsPlz')
     }
@@ -53,6 +83,24 @@ const Dashboard = (props) => {
                             <Button variant="contained" color="primary">Make Deposit</Button>
                             <Button onClick={logout} variant="contained" color="primary">Logout</Button>
                             <Button onClick={getPorts} variant="contained" color="primary">Connect Scanner</Button>
+                            {values.portOptions ?
+                                <FormControl variant="outlined" className={classes.formControl}>
+                                    <InputLabel ref={inputLabel} id="demo-simple-select-outlined-label">
+                                        Select Port</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-outlined-label"
+                                        id="demo-simple-select-outlined"
+                                        onChange={handleChange}
+                                        labelWidth={labelWidth}
+                                        value={values.port || ""}
+                                    >
+                                        {values.portOptions.map((port, index) => {
+                                            return <MenuItem key={index} value={port}>{port}</MenuItem>
+                                        })}
+                                    </Select>
+                                </FormControl>
+                                : null
+                            }
                         </div>
             }
 
