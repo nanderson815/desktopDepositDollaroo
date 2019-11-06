f6241fb704793ce808bba50fa8991ea8f29f4f70
Noah A <nanderson815@gmail.com>
2019-11-06 05:52:14 -0500

    reading duplicates and unique bills

--- src/Components/DepositFuncs.js
+++ src/Components/DepositFuncs.js
@@ -1,21 +1,30 @@
 
 
-const checkBills = (bills) => {
-    console.log(bills)
+const checkBills = (bills, db) => {
+    let duplicates = [];
+    let unique = [];
+    bills.forEach((bill) => {
+        let docRef = db.collection('submittedBills').doc(bill.serial);
+        docRef.get().then((doc) => {
+            if (doc.exists) {
+                duplicates.push(bill);
+            } else {
+                unique.push(bill)
+            }
+        })
+    })
+    return {
+        unique: unique,
+        duplicates: duplicates
+    }
 };
 
 const submitTran = async (bills, firebase) => {
     let db = firebase.firestore()
-    let submittedBills = []
-    db.collection('submittedBills').get().then((snap) => {
-        snap.forEach((doc) => {
-            submittedBills.push(doc.data())
-        })
-    })
-    console.log(submittedBills);
+    let sortedBills = await checkBills(bills, db);
+    console.log(sortedBills);
 }
 
 module.exports = {
-    checkBills: checkBills,
     submitTran: submitTran
 }
