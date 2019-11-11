512dcaedad156adecd3cee8f0a24f676b02025e1
Noah A <nanderson815@gmail.com>
2019-11-11 14:38:41 -0500

    completed deposit flow

--- src/Components/DepositTables/DepositConfirmation.js
+++ src/Components/DepositTables/DepositConfirmation.js
@@ -5,9 +5,10 @@ import { Button } from '@material-ui/core';
 const DepositConfirmation = (props) => {
     return (
         <div>
-            <p>Deposit complete. You deposited ${props.amount}</p>
-            <Button onClick={props.reset}>Complete</Button>
-        </div>
+            <p>Deposit complete! The following amount has been submitted to Dollaroo.</p>
+            <p style={{ color: "green", fontWeight: "bold", fontSize: "16px", textAlign: 'center' }}> ${props.amount}</p>
+            <Button variant="contained" color="primary" onClick={props.reset}>Complete</Button>
+        </div >
     )
 }
 
